828f61f40321efba01158c395048ac239aa540d8
code34 <nicolas_boiteux@yahoo.fr>
2017-01-29 19:08:31 +0100

    add view distance add turbo mode

--- ui/mission_params.hpp
+++ ui/mission_params.hpp
@@ -21,12 +21,33 @@ class Params
 		texts[] = {"5 - easy game", "10","15","20", "25","30 - hard game"};
 		default = 15;
 	};
+	class wcviewdistance
+	{
+		title = "PLAYER VIEW DISTANCE";
+		values[] = {1500,2000,2500,3000,3500,4000,4500,5000};
+		texts[] = {"1500","2000","2500","3000","3500", "4000", "4500", "5000"};
+		default = 1500;
+	};
+	class wcvehicleviewdistance
+	{
+		title = "VEHICLE VIEW DISTANCE";
+		values[] = {1500,2000,2500,3000,3500,4000,4500,5000};
+		texts[] = {"1500","2000","2500","3000","3500", "4000", "4500", "5000"};
+		default = 3000;
+	};
 	class wcnumberofticket
 	{
 		title = "NUMBER OF TICKETS";
 		values[] = {200,500,1000,5000};
 		texts[] = {"200 - very short game", "500 - normal game","1000 - long game", "5000 - very long game"};
 		default = 500;
+	};
+	class wcspeedcoeef
+	{
+		title = "PLAYER TURBO";
+		values[] = {1, 2};
+		texts[] = {"ON", "OFF"};
+		default = 1;
 	};
 	class wcfatigue
 	{
